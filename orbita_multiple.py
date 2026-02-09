f308e133c558ebc6c375600a46b0be6e64908b93
DanielTechCL <dpincheiral89@gmail.com>
2026-02-08 21:36:31 -0300

    Add files via upload

---
+++ orbita_multiple.py
@@ -0,0 +1,30 @@
+import numpy as np  
+import matplotlib.pyplot as plt  
+
+num_orbitas = int(input('Cuántas órbitas quieres simular\n'))
+colores = ['red', 'yellow', 'blue', 'orange', 'green', 'purple']
+
+for i in range(num_orbitas):
+    radio = float(input('Radio para órbita {i=1} (km): '))
+    angulos = np.linspace(0, 2*np.pi, 1000)
+
+    if radio <= 2000:
+        print('Órbita {i=1}: Baja sobre el mar')
+    elif radio >= 1500001:
+        print('Órbita {i=1}: Fuera de Órbita')
+    elif radio >= 35787:
+        print('Órbita {i=1}: Alta sobre el mar')
+    elif radio >= 2001:
+        print('Órbita {i=1}: Media sobre el mar')
+
+    x = radio * np.cos(angulos)  
+    y = radio * np.sin(angulos)  
+
+    plt.plot(x, y, color=colores[i % len(colores)], label=f'Órbita {i+1}: {radio} km')  
+    plt.title('Comparación de múltiples órbitas')  
+    plt.xlabel('X (km)',
+            fontsize=11)  
+    plt.ylabel('Y (km)',
+            fontsize=11)  
+    plt.axis('equal')  
+    plt.show()
